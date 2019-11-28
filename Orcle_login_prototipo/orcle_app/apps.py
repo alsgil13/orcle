ac3d5e0e184bea3b4ffb1f9d3880e4a280cddc77
alsgil13 <als.gil13@gmail.com>
2019-11-28 14:34:26 -0300

    deleta

--- Orcle_login_prototipo/orcle_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrcleAppConfig(AppConfig):
-    name = 'orcle_app'
--- Orcle_login_prototipo/orcle_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class login_form(forms.Form):
-    email= forms.EmailField()
-    senha= forms.CharField()
-
-  
-    
-    
