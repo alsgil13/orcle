fccf493433057b7f25666ffd1b70227f3df5afcb
alsgil13 <als.gil13@gmail.com>
2019-12-03 17:05:27 -0300

    Insere ProcFile

--- orcle_django/orcle_django/settings.py
+++ orcle_django/orcle_django/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%_tm6=43s@m)2m@gwf&ofn8n@l&5+-qm96e=4=wci)+2lj868('
+#SECRET_KEY = '%_tm6=43s@m)2m@gwf&ofn8n@l&5+-qm96e=4=wci)+2lj868('
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '%_tm6=43s@m)2m@gwf&ofn8n@l&5+-qm96e=4=wci)+2lj868(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
